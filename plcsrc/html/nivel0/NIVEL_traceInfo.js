ced161940b16c21325deb7c350db6cc973c7e3e1
Rodrigo Goñi <58596842+RodrigoGoni@users.noreply.github.com>
2022-01-12 17:40:09 -0300

    Nivel0partecodesys

---
+++ plcsrc/html/nivel0/NIVEL_traceInfo.js
@@ -0,0 +1,378 @@
+function RTW_Sid2UrlHash() {
+	this.urlHashMap = new Array();
+	/* <S1>:99 */
+	this.urlHashMap["nivel0:1:99"] = "nivel0.exp:366";
+	/* <S1>:69 */
+	this.urlHashMap["nivel0:1:69"] = "nivel0.exp:334";
+	/* <S1>:3 */
+	this.urlHashMap["nivel0:1:3"] = "nivel0.exp:111,115,235";
+	/* <S1>:11 */
+	this.urlHashMap["nivel0:1:11"] = "nivel0.exp:150,158";
+	/* <S1>:9 */
+	this.urlHashMap["nivel0:1:9"] = "nivel0.exp:153,195";
+	/* <S1>:70 */
+	this.urlHashMap["nivel0:1:70"] = "nivel0.exp:118,182,189,238";
+	/* <S1>:35 */
+	this.urlHashMap["nivel0:1:35"] = "nivel0.exp:90,128,227,231";
+	/* <S1>:46 */
+	this.urlHashMap["nivel0:1:46"] = "nivel0.exp:105,143,302";
+	/* <S1>:47 */
+	this.urlHashMap["nivel0:1:47"] = "nivel0.exp:91,129,232,249";
+	/* <S1>:44 */
+	this.urlHashMap["nivel0:1:44"] = "nivel0.exp:252,296";
+	/* <S1>:45 */
+	this.urlHashMap["nivel0:1:45"] = "nivel0.exp:257,265,275";
+	/* <S1>:43 */
+	this.urlHashMap["nivel0:1:43"] = "nivel0.exp:270,288";
+	/* <S1>:42 */
+	this.urlHashMap["nivel0:1:42"] = "nivel0.exp:94,132,283";
+	/* <S1>:2 */
+	this.urlHashMap["nivel0:1:2"] = "nivel0.exp:88";
+	/* <S1>:18 */
+	this.urlHashMap["nivel0:1:18"] = "nivel0.exp:230";
+	/* <S1>:19 */
+	this.urlHashMap["nivel0:1:19"] = "nivel0.exp:114";
+	/* <S1>:37 */
+	this.urlHashMap["nivel0:1:37"] = "nivel0.exp:92,130";
+	/* <S1>:38 */
+	this.urlHashMap["nivel0:1:38"] = "nivel0.exp:286";
+	/* <S1>:39 */
+	this.urlHashMap["nivel0:1:39"] = "nivel0.exp:294";
+	/* <S1>:40 */
+	this.urlHashMap["nivel0:1:40"] = "nivel0.exp:273";
+	/* <S1>:41 */
+	this.urlHashMap["nivel0:1:41"] = "nivel0.exp:255";
+	/* <S1>:71 */
+	this.urlHashMap["nivel0:1:71"] = "nivel0.exp:236";
+	/* <S1>:72 */
+	this.urlHashMap["nivel0:1:72"] = "nivel0.exp:188";
+	/* <S1>:27 */
+	this.urlHashMap["nivel0:1:27"] = "nivel0.exp:156";
+	/* <S1>:42:2 */
+	this.urlHashMap["nivel0:1:42:2"] = "nivel0.exp:95,133";
+	/* <S1>:42:3 */
+	this.urlHashMap["nivel0:1:42:3"] = "nivel0.exp:97,135";
+	/* <S1>:42:4 */
+	this.urlHashMap["nivel0:1:42:4"] = "nivel0.exp:99,137";
+	/* <S1>:42:5 */
+	this.urlHashMap["nivel0:1:42:5"] = "nivel0.exp:101,139";
+	/* <S1>:42:6 */
+	this.urlHashMap["nivel0:1:42:6"] = "nivel0.exp:103,141";
+	/* <S1>:46:3 */
+	this.urlHashMap["nivel0:1:46:3"] = "nivel0.exp:106,144";
+	/* <S1>:19:1 */
+	this.urlHashMap["nivel0:1:19:1"] = "nivel0.exp:112";
+	/* <S1>:70:13 */
+	this.urlHashMap["nivel0:1:70:13"] = "nivel0.exp:119,190";
+	/* <S1>:70:14 */
+	this.urlHashMap["nivel0:1:70:14"] = "nivel0.exp:121,192";
+	/* <S1>:27:1 */
+	this.urlHashMap["nivel0:1:27:1"] = "nivel0.exp:154";
+	/* <S1>:11:3 */
+	this.urlHashMap["nivel0:1:11:3"] = "nivel0.exp:159";
+	/* <S1>:11:4 */
+	this.urlHashMap["nivel0:1:11:4"] = "nivel0.exp:161";
+	/* <S1>:11:5 */
+	this.urlHashMap["nivel0:1:11:5"] = "nivel0.exp:163";
+	/* <S1>:11:6 */
+	this.urlHashMap["nivel0:1:11:6"] = "nivel0.exp:165";
+	/* <S1>:9:9 */
+	this.urlHashMap["nivel0:1:9:9"] = "nivel0.exp:168";
+	/* <S1>:9:10 */
+	this.urlHashMap["nivel0:1:9:10"] = "nivel0.exp:169";
+	/* <S1>:9:11 */
+	this.urlHashMap["nivel0:1:9:11"] = "nivel0.exp:173";
+	/* <S1>:9:12 */
+	this.urlHashMap["nivel0:1:9:12"] = "nivel0.exp:175";
+	/* <S1>:9:13 */
+	this.urlHashMap["nivel0:1:9:13"] = "nivel0.exp:177";
+	/* <S1>:72:1 */
+	this.urlHashMap["nivel0:1:72:1"] = "nivel0.exp:183";
+	/* <S1>:9:3 */
+	this.urlHashMap["nivel0:1:9:3"] = "nivel0.exp:196";
+	/* <S1>:9:4 */
+	this.urlHashMap["nivel0:1:9:4"] = "nivel0.exp:198";
+	/* <S1>:9:5 */
+	this.urlHashMap["nivel0:1:9:5"] = "nivel0.exp:200";
+	/* <S1>:9:6 */
+	this.urlHashMap["nivel0:1:9:6"] = "nivel0.exp:202";
+	/* <S1>:9:7 */
+	this.urlHashMap["nivel0:1:9:7"] = "nivel0.exp:203";
+	/* <S1>:70:9 */
+	this.urlHashMap["nivel0:1:70:9"] = "nivel0.exp:216";
+	/* <S1>:70:10 */
+	this.urlHashMap["nivel0:1:70:10"] = "nivel0.exp:217";
+	/* <S1>:70:11 */
+	this.urlHashMap["nivel0:1:70:11"] = "nivel0.exp:221";
+	/* <S1>:18:1 */
+	this.urlHashMap["nivel0:1:18:1"] = "nivel0.exp:228";
+	/* <S1>:70:3 */
+	this.urlHashMap["nivel0:1:70:3"] = "nivel0.exp:239";
+	/* <S1>:70:4 */
+	this.urlHashMap["nivel0:1:70:4"] = "nivel0.exp:241";
+	/* <S1>:70:5 */
+	this.urlHashMap["nivel0:1:70:5"] = "nivel0.exp:243";
+	/* <S1>:70:6 */
+	this.urlHashMap["nivel0:1:70:6"] = "nivel0.exp:244";
+	/* <S1>:70:7 */
+	this.urlHashMap["nivel0:1:70:7"] = "nivel0.exp:246";
+	/* <S1>:41:1 */
+	this.urlHashMap["nivel0:1:41:1"] = "nivel0.exp:253";
+	/* <S1>:45:2 */
+	this.urlHashMap["nivel0:1:45:2"] = "nivel0.exp:258,266,276";
+	/* <S1>:44:2 */
+	this.urlHashMap["nivel0:1:44:2"] = "nivel0.exp:261,297";
+	/* <S1>:40:1 */
+	this.urlHashMap["nivel0:1:40:1"] = "nivel0.exp:271";
+	/* <S1>:38:1 */
+	this.urlHashMap["nivel0:1:38:1"] = "nivel0.exp:284";
+	/* <S1>:43:3 */
+	this.urlHashMap["nivel0:1:43:3"] = "nivel0.exp:289";
+	/* <S1>:39:1 */
+	this.urlHashMap["nivel0:1:39:1"] = "nivel0.exp:292";
+	/* <S1>:46:5 */
+	this.urlHashMap["nivel0:1:46:5"] = "nivel0.exp:303";
+	/* <S1>:46:6 */
+	this.urlHashMap["nivel0:1:46:6"] = "nivel0.exp:305";
+	/* <S1>:46:8 */
+	this.urlHashMap["nivel0:1:46:8"] = "nivel0.exp:308";
+	/* <S1>:46:9 */
+	this.urlHashMap["nivel0:1:46:9"] = "nivel0.exp:310";
+	/* <S1>:46:11 */
+	this.urlHashMap["nivel0:1:46:11"] = "nivel0.exp:313";
+	/* <S1>:46:12 */
+	this.urlHashMap["nivel0:1:46:12"] = "nivel0.exp:315";
+	/* <S1>:69:2 */
+	this.urlHashMap["nivel0:1:69:2"] = "nivel0.exp:335";
+	/* <S1>:69:3 */
+	this.urlHashMap["nivel0:1:69:3"] = "nivel0.exp:337";
+	/* <S1>:69:4 */
+	this.urlHashMap["nivel0:1:69:4"] = "nivel0.exp:340";
+	/* <S1>:69:5 */
+	this.urlHashMap["nivel0:1:69:5"] = "nivel0.exp:341";
+	/* <S1>:69:7 */
+	this.urlHashMap["nivel0:1:69:7"] = "nivel0.exp:344";
+	/* <S1>:69:8 */
+	this.urlHashMap["nivel0:1:69:8"] = "nivel0.exp:346";
+	/* <S1>:69:9 */
+	this.urlHashMap["nivel0:1:69:9"] = "nivel0.exp:349";
+	/* <S1>:69:10 */
+	this.urlHashMap["nivel0:1:69:10"] = "nivel0.exp:350";
+	/* <S1>:99:2 */
+	this.urlHashMap["nivel0:1:99:2"] = "nivel0.exp:367";
+	/* <S1>:99:3 */
+	this.urlHashMap["nivel0:1:99:3"] = "nivel0.exp:369";
+	/* <S1>:99:4 */
+	this.urlHashMap["nivel0:1:99:4"] = "nivel0.exp:372";
+	/* <S1>:99:5 */
+	this.urlHashMap["nivel0:1:99:5"] = "nivel0.exp:373";
+	/* <S1>:99:6 */
+	this.urlHashMap["nivel0:1:99:6"] = "nivel0.exp:376";
+	/* <S1>:99:7 */
+	this.urlHashMap["nivel0:1:99:7"] = "nivel0.exp:377";
+	/* <S1>:99:9 */
+	this.urlHashMap["nivel0:1:99:9"] = "nivel0.exp:380";
+	/* <S1>:99:10 */
+	this.urlHashMap["nivel0:1:99:10"] = "nivel0.exp:382";
+	/* <S1>:99:11 */
+	this.urlHashMap["nivel0:1:99:11"] = "nivel0.exp:385";
+	/* <S1>:99:12 */
+	this.urlHashMap["nivel0:1:99:12"] = "nivel0.exp:386";
+	/* <S1>:99:13 */
+	this.urlHashMap["nivel0:1:99:13"] = "nivel0.exp:389";
+	/* <S1>:99:14 */
+	this.urlHashMap["nivel0:1:99:14"] = "nivel0.exp:390";
+	this.getUrlHash = function(sid) { return this.urlHashMap[sid];}
+}
+RTW_Sid2UrlHash.instance = new RTW_Sid2UrlHash();
+function RTW_rtwnameSIDMap() {
+	this.rtwnameHashMap = new Array();
+	this.sidHashMap = new Array();
+	this.rtwnameHashMap["<Root>"] = {sid: "nivel0"};
+	this.sidHashMap["nivel0"] = {rtwname: "<Root>"};
+	this.rtwnameHashMap["<S1>:99"] = {sid: "nivel0:1:99"};
+	this.sidHashMap["nivel0:1:99"] = {rtwname: "<S1>:99"};
+	this.rtwnameHashMap["<S1>:69"] = {sid: "nivel0:1:69"};
+	this.sidHashMap["nivel0:1:69"] = {rtwname: "<S1>:69"};
+	this.rtwnameHashMap["<S1>:3"] = {sid: "nivel0:1:3"};
+	this.sidHashMap["nivel0:1:3"] = {rtwname: "<S1>:3"};
+	this.rtwnameHashMap["<S1>:11"] = {sid: "nivel0:1:11"};
+	this.sidHashMap["nivel0:1:11"] = {rtwname: "<S1>:11"};
+	this.rtwnameHashMap["<S1>:9"] = {sid: "nivel0:1:9"};
+	this.sidHashMap["nivel0:1:9"] = {rtwname: "<S1>:9"};
+	this.rtwnameHashMap["<S1>:70"] = {sid: "nivel0:1:70"};
+	this.sidHashMap["nivel0:1:70"] = {rtwname: "<S1>:70"};
+	this.rtwnameHashMap["<S1>:35"] = {sid: "nivel0:1:35"};
+	this.sidHashMap["nivel0:1:35"] = {rtwname: "<S1>:35"};
+	this.rtwnameHashMap["<S1>:46"] = {sid: "nivel0:1:46"};
+	this.sidHashMap["nivel0:1:46"] = {rtwname: "<S1>:46"};
+	this.rtwnameHashMap["<S1>:47"] = {sid: "nivel0:1:47"};
+	this.sidHashMap["nivel0:1:47"] = {rtwname: "<S1>:47"};
+	this.rtwnameHashMap["<S1>:44"] = {sid: "nivel0:1:44"};
+	this.sidHashMap["nivel0:1:44"] = {rtwname: "<S1>:44"};
+	this.rtwnameHashMap["<S1>:45"] = {sid: "nivel0:1:45"};
+	this.sidHashMap["nivel0:1:45"] = {rtwname: "<S1>:45"};
+	this.rtwnameHashMap["<S1>:43"] = {sid: "nivel0:1:43"};
+	this.sidHashMap["nivel0:1:43"] = {rtwname: "<S1>:43"};
+	this.rtwnameHashMap["<S1>:42"] = {sid: "nivel0:1:42"};
+	this.sidHashMap["nivel0:1:42"] = {rtwname: "<S1>:42"};
+	this.rtwnameHashMap["<S1>:2"] = {sid: "nivel0:1:2"};
+	this.sidHashMap["nivel0:1:2"] = {rtwname: "<S1>:2"};
+	this.rtwnameHashMap["<S1>:18"] = {sid: "nivel0:1:18"};
+	this.sidHashMap["nivel0:1:18"] = {rtwname: "<S1>:18"};
+	this.rtwnameHashMap["<S1>:19"] = {sid: "nivel0:1:19"};
+	this.sidHashMap["nivel0:1:19"] = {rtwname: "<S1>:19"};
+	this.rtwnameHashMap["<S1>:37"] = {sid: "nivel0:1:37"};
+	this.sidHashMap["nivel0:1:37"] = {rtwname: "<S1>:37"};
+	this.rtwnameHashMap["<S1>:38"] = {sid: "nivel0:1:38"};
+	this.sidHashMap["nivel0:1:38"] = {rtwname: "<S1>:38"};
+	this.rtwnameHashMap["<S1>:39"] = {sid: "nivel0:1:39"};
+	this.sidHashMap["nivel0:1:39"] = {rtwname: "<S1>:39"};
+	this.rtwnameHashMap["<S1>:40"] = {sid: "nivel0:1:40"};
+	this.sidHashMap["nivel0:1:40"] = {rtwname: "<S1>:40"};
+	this.rtwnameHashMap["<S1>:41"] = {sid: "nivel0:1:41"};
+	this.sidHashMap["nivel0:1:41"] = {rtwname: "<S1>:41"};
+	this.rtwnameHashMap["<S1>:71"] = {sid: "nivel0:1:71"};
+	this.sidHashMap["nivel0:1:71"] = {rtwname: "<S1>:71"};
+	this.rtwnameHashMap["<S1>:72"] = {sid: "nivel0:1:72"};
+	this.sidHashMap["nivel0:1:72"] = {rtwname: "<S1>:72"};
+	this.rtwnameHashMap["<S1>:27"] = {sid: "nivel0:1:27"};
+	this.sidHashMap["nivel0:1:27"] = {rtwname: "<S1>:27"};
+	this.rtwnameHashMap["<S1>:42:2"] = {sid: "nivel0:1:42:2"};
+	this.sidHashMap["nivel0:1:42:2"] = {rtwname: "<S1>:42:2"};
+	this.rtwnameHashMap["<S1>:42:3"] = {sid: "nivel0:1:42:3"};
+	this.sidHashMap["nivel0:1:42:3"] = {rtwname: "<S1>:42:3"};
+	this.rtwnameHashMap["<S1>:42:4"] = {sid: "nivel0:1:42:4"};
+	this.sidHashMap["nivel0:1:42:4"] = {rtwname: "<S1>:42:4"};
+	this.rtwnameHashMap["<S1>:42:5"] = {sid: "nivel0:1:42:5"};
+	this.sidHashMap["nivel0:1:42:5"] = {rtwname: "<S1>:42:5"};
+	this.rtwnameHashMap["<S1>:42:6"] = {sid: "nivel0:1:42:6"};
+	this.sidHashMap["nivel0:1:42:6"] = {rtwname: "<S1>:42:6"};
+	this.rtwnameHashMap["<S1>:46:3"] = {sid: "nivel0:1:46:3"};
+	this.sidHashMap["nivel0:1:46:3"] = {rtwname: "<S1>:46:3"};
+	this.rtwnameHashMap["<S1>:19:1"] = {sid: "nivel0:1:19:1"};
+	this.sidHashMap["nivel0:1:19:1"] = {rtwname: "<S1>:19:1"};
+	this.rtwnameHashMap["<S1>:70:13"] = {sid: "nivel0:1:70:13"};
+	this.sidHashMap["nivel0:1:70:13"] = {rtwname: "<S1>:70:13"};
+	this.rtwnameHashMap["<S1>:70:14"] = {sid: "nivel0:1:70:14"};
+	this.sidHashMap["nivel0:1:70:14"] = {rtwname: "<S1>:70:14"};
+	this.rtwnameHashMap["<S1>:27:1"] = {sid: "nivel0:1:27:1"};
+	this.sidHashMap["nivel0:1:27:1"] = {rtwname: "<S1>:27:1"};
+	this.rtwnameHashMap["<S1>:11:3"] = {sid: "nivel0:1:11:3"};
+	this.sidHashMap["nivel0:1:11:3"] = {rtwname: "<S1>:11:3"};
+	this.rtwnameHashMap["<S1>:11:4"] = {sid: "nivel0:1:11:4"};
+	this.sidHashMap["nivel0:1:11:4"] = {rtwname: "<S1>:11:4"};
+	this.rtwnameHashMap["<S1>:11:5"] = {sid: "nivel0:1:11:5"};
+	this.sidHashMap["nivel0:1:11:5"] = {rtwname: "<S1>:11:5"};
+	this.rtwnameHashMap["<S1>:11:6"] = {sid: "nivel0:1:11:6"};
+	this.sidHashMap["nivel0:1:11:6"] = {rtwname: "<S1>:11:6"};
+	this.rtwnameHashMap["<S1>:9:9"] = {sid: "nivel0:1:9:9"};
+	this.sidHashMap["nivel0:1:9:9"] = {rtwname: "<S1>:9:9"};
+	this.rtwnameHashMap["<S1>:9:10"] = {sid: "nivel0:1:9:10"};
+	this.sidHashMap["nivel0:1:9:10"] = {rtwname: "<S1>:9:10"};
+	this.rtwnameHashMap["<S1>:9:11"] = {sid: "nivel0:1:9:11"};
+	this.sidHashMap["nivel0:1:9:11"] = {rtwname: "<S1>:9:11"};
+	this.rtwnameHashMap["<S1>:9:12"] = {sid: "nivel0:1:9:12"};
+	this.sidHashMap["nivel0:1:9:12"] = {rtwname: "<S1>:9:12"};
+	this.rtwnameHashMap["<S1>:9:13"] = {sid: "nivel0:1:9:13"};
+	this.sidHashMap["nivel0:1:9:13"] = {rtwname: "<S1>:9:13"};
+	this.rtwnameHashMap["<S1>:72:1"] = {sid: "nivel0:1:72:1"};
+	this.sidHashMap["nivel0:1:72:1"] = {rtwname: "<S1>:72:1"};
+	this.rtwnameHashMap["<S1>:9:3"] = {sid: "nivel0:1:9:3"};
+	this.sidHashMap["nivel0:1:9:3"] = {rtwname: "<S1>:9:3"};
+	this.rtwnameHashMap["<S1>:9:4"] = {sid: "nivel0:1:9:4"};
+	this.sidHashMap["nivel0:1:9:4"] = {rtwname: "<S1>:9:4"};
+	this.rtwnameHashMap["<S1>:9:5"] = {sid: "nivel0:1:9:5"};
+	this.sidHashMap["nivel0:1:9:5"] = {rtwname: "<S1>:9:5"};
+	this.rtwnameHashMap["<S1>:9:6"] = {sid: "nivel0:1:9:6"};
+	this.sidHashMap["nivel0:1:9:6"] = {rtwname: "<S1>:9:6"};
+	this.rtwnameHashMap["<S1>:9:7"] = {sid: "nivel0:1:9:7"};
+	this.sidHashMap["nivel0:1:9:7"] = {rtwname: "<S1>:9:7"};
+	this.rtwnameHashMap["<S1>:70:9"] = {sid: "nivel0:1:70:9"};
+	this.sidHashMap["nivel0:1:70:9"] = {rtwname: "<S1>:70:9"};
+	this.rtwnameHashMap["<S1>:70:10"] = {sid: "nivel0:1:70:10"};
+	this.sidHashMap["nivel0:1:70:10"] = {rtwname: "<S1>:70:10"};
+	this.rtwnameHashMap["<S1>:70:11"] = {sid: "nivel0:1:70:11"};
+	this.sidHashMap["nivel0:1:70:11"] = {rtwname: "<S1>:70:11"};
+	this.rtwnameHashMap["<S1>:18:1"] = {sid: "nivel0:1:18:1"};
+	this.sidHashMap["nivel0:1:18:1"] = {rtwname: "<S1>:18:1"};
+	this.rtwnameHashMap["<S1>:70:3"] = {sid: "nivel0:1:70:3"};
+	this.sidHashMap["nivel0:1:70:3"] = {rtwname: "<S1>:70:3"};
+	this.rtwnameHashMap["<S1>:70:4"] = {sid: "nivel0:1:70:4"};
+	this.sidHashMap["nivel0:1:70:4"] = {rtwname: "<S1>:70:4"};
+	this.rtwnameHashMap["<S1>:70:5"] = {sid: "nivel0:1:70:5"};
+	this.sidHashMap["nivel0:1:70:5"] = {rtwname: "<S1>:70:5"};
+	this.rtwnameHashMap["<S1>:70:6"] = {sid: "nivel0:1:70:6"};
+	this.sidHashMap["nivel0:1:70:6"] = {rtwname: "<S1>:70:6"};
+	this.rtwnameHashMap["<S1>:70:7"] = {sid: "nivel0:1:70:7"};
+	this.sidHashMap["nivel0:1:70:7"] = {rtwname: "<S1>:70:7"};
+	this.rtwnameHashMap["<S1>:41:1"] = {sid: "nivel0:1:41:1"};
+	this.sidHashMap["nivel0:1:41:1"] = {rtwname: "<S1>:41:1"};
+	this.rtwnameHashMap["<S1>:45:2"] = {sid: "nivel0:1:45:2"};
+	this.sidHashMap["nivel0:1:45:2"] = {rtwname: "<S1>:45:2"};
+	this.rtwnameHashMap["<S1>:44:2"] = {sid: "nivel0:1:44:2"};
+	this.sidHashMap["nivel0:1:44:2"] = {rtwname: "<S1>:44:2"};
+	this.rtwnameHashMap["<S1>:40:1"] = {sid: "nivel0:1:40:1"};
+	this.sidHashMap["nivel0:1:40:1"] = {rtwname: "<S1>:40:1"};
+	this.rtwnameHashMap["<S1>:38:1"] = {sid: "nivel0:1:38:1"};
+	this.sidHashMap["nivel0:1:38:1"] = {rtwname: "<S1>:38:1"};
+	this.rtwnameHashMap["<S1>:43:3"] = {sid: "nivel0:1:43:3"};
+	this.sidHashMap["nivel0:1:43:3"] = {rtwname: "<S1>:43:3"};
+	this.rtwnameHashMap["<S1>:39:1"] = {sid: "nivel0:1:39:1"};
+	this.sidHashMap["nivel0:1:39:1"] = {rtwname: "<S1>:39:1"};
+	this.rtwnameHashMap["<S1>:46:5"] = {sid: "nivel0:1:46:5"};
+	this.sidHashMap["nivel0:1:46:5"] = {rtwname: "<S1>:46:5"};
+	this.rtwnameHashMap["<S1>:46:6"] = {sid: "nivel0:1:46:6"};
+	this.sidHashMap["nivel0:1:46:6"] = {rtwname: "<S1>:46:6"};
+	this.rtwnameHashMap["<S1>:46:8"] = {sid: "nivel0:1:46:8"};
+	this.sidHashMap["nivel0:1:46:8"] = {rtwname: "<S1>:46:8"};
+	this.rtwnameHashMap["<S1>:46:9"] = {sid: "nivel0:1:46:9"};
+	this.sidHashMap["nivel0:1:46:9"] = {rtwname: "<S1>:46:9"};
+	this.rtwnameHashMap["<S1>:46:11"] = {sid: "nivel0:1:46:11"};
+	this.sidHashMap["nivel0:1:46:11"] = {rtwname: "<S1>:46:11"};
+	this.rtwnameHashMap["<S1>:46:12"] = {sid: "nivel0:1:46:12"};
+	this.sidHashMap["nivel0:1:46:12"] = {rtwname: "<S1>:46:12"};
+	this.rtwnameHashMap["<S1>:69:2"] = {sid: "nivel0:1:69:2"};
+	this.sidHashMap["nivel0:1:69:2"] = {rtwname: "<S1>:69:2"};
+	this.rtwnameHashMap["<S1>:69:3"] = {sid: "nivel0:1:69:3"};
+	this.sidHashMap["nivel0:1:69:3"] = {rtwname: "<S1>:69:3"};
+	this.rtwnameHashMap["<S1>:69:4"] = {sid: "nivel0:1:69:4"};
+	this.sidHashMap["nivel0:1:69:4"] = {rtwname: "<S1>:69:4"};
+	this.rtwnameHashMap["<S1>:69:5"] = {sid: "nivel0:1:69:5"};
+	this.sidHashMap["nivel0:1:69:5"] = {rtwname: "<S1>:69:5"};
+	this.rtwnameHashMap["<S1>:69:7"] = {sid: "nivel0:1:69:7"};
+	this.sidHashMap["nivel0:1:69:7"] = {rtwname: "<S1>:69:7"};
+	this.rtwnameHashMap["<S1>:69:8"] = {sid: "nivel0:1:69:8"};
+	this.sidHashMap["nivel0:1:69:8"] = {rtwname: "<S1>:69:8"};
+	this.rtwnameHashMap["<S1>:69:9"] = {sid: "nivel0:1:69:9"};
+	this.sidHashMap["nivel0:1:69:9"] = {rtwname: "<S1>:69:9"};
+	this.rtwnameHashMap["<S1>:69:10"] = {sid: "nivel0:1:69:10"};
+	this.sidHashMap["nivel0:1:69:10"] = {rtwname: "<S1>:69:10"};
+	this.rtwnameHashMap["<S1>:99:2"] = {sid: "nivel0:1:99:2"};
+	this.sidHashMap["nivel0:1:99:2"] = {rtwname: "<S1>:99:2"};
+	this.rtwnameHashMap["<S1>:99:3"] = {sid: "nivel0:1:99:3"};
+	this.sidHashMap["nivel0:1:99:3"] = {rtwname: "<S1>:99:3"};
+	this.rtwnameHashMap["<S1>:99:4"] = {sid: "nivel0:1:99:4"};
+	this.sidHashMap["nivel0:1:99:4"] = {rtwname: "<S1>:99:4"};
+	this.rtwnameHashMap["<S1>:99:5"] = {sid: "nivel0:1:99:5"};
+	this.sidHashMap["nivel0:1:99:5"] = {rtwname: "<S1>:99:5"};
+	this.rtwnameHashMap["<S1>:99:6"] = {sid: "nivel0:1:99:6"};
+	this.sidHashMap["nivel0:1:99:6"] = {rtwname: "<S1>:99:6"};
+	this.rtwnameHashMap["<S1>:99:7"] = {sid: "nivel0:1:99:7"};
+	this.sidHashMap["nivel0:1:99:7"] = {rtwname: "<S1>:99:7"};
+	this.rtwnameHashMap["<S1>:99:9"] = {sid: "nivel0:1:99:9"};
+	this.sidHashMap["nivel0:1:99:9"] = {rtwname: "<S1>:99:9"};
+	this.rtwnameHashMap["<S1>:99:10"] = {sid: "nivel0:1:99:10"};
+	this.sidHashMap["nivel0:1:99:10"] = {rtwname: "<S1>:99:10"};
+	this.rtwnameHashMap["<S1>:99:11"] = {sid: "nivel0:1:99:11"};
+	this.sidHashMap["nivel0:1:99:11"] = {rtwname: "<S1>:99:11"};
+	this.rtwnameHashMap["<S1>:99:12"] = {sid: "nivel0:1:99:12"};
+	this.sidHashMap["nivel0:1:99:12"] = {rtwname: "<S1>:99:12"};
+	this.rtwnameHashMap["<S1>:99:13"] = {sid: "nivel0:1:99:13"};
+	this.sidHashMap["nivel0:1:99:13"] = {rtwname: "<S1>:99:13"};
+	this.rtwnameHashMap["<S1>:99:14"] = {sid: "nivel0:1:99:14"};
+	this.sidHashMap["nivel0:1:99:14"] = {rtwname: "<S1>:99:14"};
+	this.getSID = function(rtwname) { return this.rtwnameHashMap[rtwname];}
+	this.getRtwname = function(sid) { return this.sidHashMap[sid];}
+}
+RTW_rtwnameSIDMap.instance = new RTW_rtwnameSIDMap();
